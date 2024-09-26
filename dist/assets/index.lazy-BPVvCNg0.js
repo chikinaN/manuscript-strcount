d711503417c961e6e22621b036fbf2975fe41536
chigayuki <yukinari_22n1101244@nnn.ed.jp>
2024-09-26 18:46:46 +0900

    dict

---
+++ dist/assets/index.lazy-BPVvCNg0.js
@@ -0,0 +1,1 @@
+import{c as t,j as e}from"./index-CvhBCLOr.js";const c=t("/manuscript-strcount/")({component:o});function o(){return e.jsx("div",{className:"p-2",children:e.jsx("h3",{children:"Welcome Home!"})})}export{c as Route};
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react'
 import { Outlet, createRootRoute } from '@tanstack/react-router'
 import { Layout } from '../components/Layout'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
