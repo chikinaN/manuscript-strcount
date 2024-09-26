1148df2f7f0182875452557373973240b2f11c59
chigayuki <yukinari_22n1101244@nnn.ed.jp>
2024-09-26 19:15:33 +0900

    二個あったわ

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,11 @@ import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 import { router } from './route'
 import './index.css'
 
+const dev = import.meta.env.DEV
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router={router} />
-    <TanStackRouterDevtools router={router} />
+    {dev && <TanStackRouterDevtools router={router} />}
   </StrictMode>,
 )
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,16 +1,10 @@
 import { Outlet, createRootRoute } from '@tanstack/react-router'
 import { Layout } from '../components/Layout'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 
 export const Route = createRootRoute({
-  component: () => {
-    const dev = import.meta.env.DEV
-    console.log('dev', dev)
-    return (
-      <Layout>
-        <Outlet />
-        {dev && <TanStackRouterDevtools />}
-      </Layout>
-    )
-  }
+  component: () => (
+    <Layout>
+      <Outlet />
+    </Layout>
+  )
 })
